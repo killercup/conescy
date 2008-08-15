0834a44da5090400930ada8b8363012f57bfa7d7
Pascal <killercup@gmail.com>
2008-08-15 18:58:26 +0200

    Adds conescy.toolbox where I'm going to store some coole utilities and tools which can be used in all apps.
    * Currently only contains some search template tags, but they are very useful!

---
+++ conescy/toolbox/__init__.py
@@ -0,0 +1,3 @@
+"""
+This is a collection of simple tools and some defaults for conescy. Notice that not everything here is written by me, but the copyright of foreign scripts is often marked on top of that files.
+"""
